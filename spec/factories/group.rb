8aefe8d1cb2cbfb015f8fc61f35315a3d21bc6a7
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-09-03 14:54:29 +0900

    テスト用gemインストール+Factory設定

---
+++ spec/factories/group.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  
+  factory :gruop do
+    name{Fake::Team.name}  #複数のダミーデータを作成
+  end
+
+end
---
+++ spec/factories/messages.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  
+  factory :message do
+    contents         {Faker::Lorem.sentence}#Fakerでダミーデータ作成（短文）※複数のデータを作れる
+    image            {File.open("#{Rails.root}/public/image/test_image.jpg")}
+                                 #↑Rails.rootの意味は/Users/~~/アプリケーションまでのパスを取得
+                                 #ダミー画像を設定
+    user   #user_idが反映
+    group  #group_idが反映
+  end
+end
